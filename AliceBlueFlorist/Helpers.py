593d70cd0d3ad853d205388496a36c5f5cbf4dd2
Your Name <meir.bar.shay@gmail.com>
2022-06-06 11:29:12 +0300

    AliceBlueFlorist-Key-private

--- AliceBlueFlorist/Helpers.py
+++ AliceBlueFlorist/Helpers.py
@@ -8,7 +8,7 @@ input_em = "(//input[@autocomplete='off'])[2]"
 input_pas = "//input[@type='password']"
 error_png = "aliceblueflorist_page_loading_error.png"
 Title = "AliceBlue Florist"
-key = 'https://meirbarshay_j1pXRy:9JNPeY1s7y8kdqqsyMQG@hub-cloud.browserstack.com/wd/hub'
+key = 'https://meirbarshay_"---keyBrowserStack----"@hub-cloud.browserstack.com/wd/hub'
 
 
 # driver sleep from 2 to 4 seconds
